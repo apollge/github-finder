70e638d8dddcdfdcdf2ed816202457d9453a87ee
jan <apollg3@gmail.com>
2019-10-05 17:35:55 +0800

    Moved User State to Context

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -13,10 +13,22 @@ export default (state, action) => {
         ...state,
         loading: true
       };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
     case SEARCH_USERS:
       return {
         ...state,
         users: action.payload,
+        loading: false
+      };
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
         loading: false
       };
     default:
